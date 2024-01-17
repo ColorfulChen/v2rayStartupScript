b0cb8e88d3cc628d009ac94901b50e3843477011
cyk <35164678+MrChenYukun@users.noreply.github.com>
2024-01-17 15:38:59 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-#script from https://github.com/233boy/v2ray/tree/master
-wget https://git.io/v2ray.sh
-echo -e "1\n13\n8080\n\n\n\n\n" > v2ray_config.txt 
-bash v2ray.sh < v2ray_config.txt
-rm -f v2ray.sh
-rm -f v2ray_config.txt
-v2ray restart
-reboot
+#fork v2ray script form 233boy
+wget https://github.com/233boy/v2ray/archive/master.tar.gz -O v2ray-master.tar.gz;tar -zxvf v2ray-master.tar.gz;cd v2ray-master;chmod +x i*;./i* -l
+
+#disable firewall
+systemctl stop firewalld
+systemctl disable firewalld
+ufw disable
